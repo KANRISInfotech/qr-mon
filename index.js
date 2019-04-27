cd98197f824c2995c7dfde9ef564fa0fec63ce3d
Rishi <rishi@kanris.biz>
2019-04-28 04:16:51 +0530

    gradient fill, background color, margin

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ var qrw = require('./lib/qr_gen');
 
 app.get('/qr', function(req, res) {
     let error = false;
-    let data = req.query.data ? req.query.data : error = true;
-    let shape = req.query.shape ? req.query.shape : 'default';
-    let color = req.query.color ? req.query.color : 'black';
+    let options = {};
+    options.data = req.query.data ? req.query.data : error = true;
+    options.shape = req.query.shape ? req.query.shape : 'default';
+    options.color = req.query.color ? req.query.color : 'black';
+    options.margin = req.query.margin ? req.query.margin : 0;
+    options.background = req.query.background ? req.query.background : 'rgba(0,0,0,0)';
+    options.fill = req.query.fill ? req.query.fill : 'solid';
     if (!error) {
-        var code = qrw.generate('1', { type: 'svg', shape: shape, color: color });
+        var code = qrw.generate(options);
         res.type('svg');
         res.send(code);
     } else {
