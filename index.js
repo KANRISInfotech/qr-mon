781d3f19cf96ffc2792cceafe256bf9f6d0f5b16
Rishi <rishi@kanris.biz>
2019-04-28 21:35:16 +0530

    additional designs

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ var qrw = require('./lib/qr_gen');
 
 app.get('/qr', function(req, res) {
     let error = false;
-    let options = {};
-    options.data = req.query.data ? req.query.data : error = true;
-    options.shape = req.query.shape ? req.query.shape : 'default';
-    options.color = req.query.color ? req.query.color : 'black';
-    options.margin = req.query.margin ? req.query.margin : 0;
-    options.background = req.query.background ? req.query.background : 'rgba(0,0,0,0)';
-    options.fill = req.query.fill ? req.query.fill : 'solid';
-    options.eye_frame = req.query.eye_frame ? req.query.eye_frame : 'default';
-    options.eye = req.query.eye ? req.query.eye : 'default';
+    let options = req.query;
+    let text = req.query.data ? req.query.data : error = true;
     if (!error) {
-        var code = qrw.generate(options);
+        var code = qrw.generate(text, options);
         res.type('svg');
         res.send(code);
     } else {
